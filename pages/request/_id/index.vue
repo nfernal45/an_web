65d1ea10dee11aeb7dbe3cf993d3f2894f4aae42
aismagilov <aismagilov@c-i-p.ru>
2019-09-23 18:49:28 +0300

    add request page

---
+++ pages/request/_id/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div
+    h1 Страница заявления по ID
+    nuxt
+</template>
+
+<script>
+export default {
+  name: 'RequestPage'
+}
+</script>
+
+<style lang="sass"></style>
