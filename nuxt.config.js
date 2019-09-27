0dc755d70eb51c798d1b9037af8bcba05129bd82
aismagilov <aismagilov@c-i-p.ru>
2019-09-27 15:03:38 +0300

    add env title variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.APP_TITLE || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -46,7 +46,8 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    ['@nuxtjs/dotenv', { path: '~/', systemvars: true }]
   ],
   /*
    ** Axios module configuration
