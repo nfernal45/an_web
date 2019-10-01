586df9d23a004550f1ce642bc737bc3809088532
aismagilov <aismagilov@c-i-p.ru>
2019-10-01 12:39:28 +0300

    MZHI-5217 add pages

---
+++ pages/appeal/_id/main/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  h1  Основная вкладка заявления {{ appealId }}
+</template>
+
+<script>
+export default {
+  name: 'AppealMainPage',
+  computed: {
+    appealId() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/request/_id/main/index.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-  h1  Основная вкладка заявления
-</template>
-
-<script>
-export default {}
-</script>
-
-<style></style>
