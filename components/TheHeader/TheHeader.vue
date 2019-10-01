1e45b3642076b5ca5841008c4fefe91e5413f19a
chikenfoot <chikenfoot88@gmail.com>
2019-10-01 19:14:25 +0300

    MZHI-5218 add TheHeader component

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  header
+    h1 the header
+    <el-button type="primary">Primary</el-button>
+</template>
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+<style lang="sass"></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <the-header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader'
+
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
 
 <style>
 html {
