a8733abf28f55e7de2485e8b15bf0ed6958d6179
aismagilov <aismagilov@c-i-p.ru>
2019-10-01 12:38:57 +0300

    MZHI-5217 replace store types

---
+++ store/types/appeal.js
@@ -0,0 +1,18 @@
+const actionTypes = {
+  FETCH_APPEAL: 'FETCH_APPEAL',
+  FETCH_APPEAL_LIST: 'FETCH_APPEAL_LIST',
+  SAVE_APPEAL: 'SAVE_APPEAL',
+  CHANGE_APPEAL_STATUS: 'CHANGE_APPEAL_STATUS'
+}
+
+const mutationTypes = {
+  SET_APPEAL: 'SET_APPEAL',
+  RESET_APPEAL: 'RESET_APPEAL',
+  RESET_APPEAL_LIST: 'RESET_APPEAL_LIST'
+}
+
+const getterTypes = {
+  GET_APPEAL: 'GET_APPEAL'
+}
+
+export { actionTypes, mutationTypes, getterTypes }
