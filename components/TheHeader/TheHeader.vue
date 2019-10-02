875d665ce1967b14e4a661159189c7b9fe0b59de
aismagilov <aismagilov@c-i-p.ru>
2019-10-02 16:41:22 +0300

    MZHI-5218 add TheHeader component

--- components/TheHeader/TheHeader.vue
+++ components/TheHeader/TheHeader.vue
@@ -1,11 +1,39 @@
 <template lang="pug">
-  header
-    h1 the header
-    <el-button type="primary">Primary</el-button>
+  el-header(:class="styles.header" height="80px")
+    el-container
+      div(class="flex align-center width-100")
+        img(src="@/assets/img/logo.png" :class="styles.logo")
+        h3(:class="styles.title") {{ title }}
+        div(:class="styles.buttons")
+          font-awesome-icon(icon="user" transform="shrink-3" size="2x" title="Мои настройки" @click="userSettings()")
+          font-awesome-icon(icon="sign-out-alt" size="2x" title="Выйти из системы" @click="logout()")
 </template>
 <script>
+import styles from './TheHeader.module.sass?module'
+
 export default {
-  name: 'TheHeader'
+  name: 'TheHeader',
+  data() {
+    return {
+      // TODO: взять значение из глобального head.title
+      title: `Подсистема "Реестр сведений о лицензиатах"
+        ЕИС МЖИ. Внесение изменений в реестр.`
+    }
+  },
+  computed: {
+    styles() {
+      return styles
+    }
+  },
+  methods: {
+    userSettings() {
+      this.$emit('userSettings')
+      alert('Функционал в разработке')
+    },
+    logout() {
+      this.$emit('logout')
+      alert('Функционал в разработке')
+    }
+  }
 }
 </script>
-<style lang="sass"></style>
