816fa23b486c19554a490f6650fbcd4a97b70a6e
aismagilov <aismagilov@c-i-p.ru>
2019-10-07 16:56:56 +0300

    MZHI-5286. TheAside. add aside component

---
+++ components/TheAside/TheAside.vue
@@ -0,0 +1,72 @@
+<template lang="pug">
+  el-aside(width="200px" :class="styles.aside")
+    ul(:class="styles['list']")
+      li(:class="styles['list-item']")
+        nuxt-link(to="/registry")
+          el-button(type="primary" :class="styles['list-button']")
+            font-awesome-icon(icon="reply")
+            span Назад к списку
+      li(:class="styles['list-item']")
+        el-button(type="success" :class="styles['list-button']" @click="saveRequest()")
+          font-awesome-icon(icon="save")
+          span Сохранить
+      li(:class="styles['list-item']")
+        el-button(type="primary" plain :class="styles['list-button']" @click="print()")
+          font-awesome-icon(icon="print")
+          span Печать
+
+    ul(:class="styles['list']")
+      li(v-for="(button, index) in statusChangeButtonsComputed" :key="index" :class="styles['list-item']")
+        el-button(type="warning" plain :class="styles['list-button']" @click="changeStatus()") {{ button.title }}
+
+</template>
+<script>
+import styles from './TheAside.module.sass?module'
+
+export default {
+  name: 'TheAside',
+  data() {
+    return {
+      appealStatus: 1,
+      statusChangeButtons: [
+        {
+          title: 'На рассмотрение',
+          activeStatuses: [1],
+          changeStatus: [2]
+        },
+        {
+          activeStatuses: [1],
+          title: 'Запросить документы',
+          changeStatus: [3]
+        },
+        {
+          title: 'Сведения получены',
+          activeStatuses: [1],
+          changeStatus: [3]
+        }
+      ]
+    }
+  },
+  computed: {
+    styles() {
+      return styles
+    },
+    statusChangeButtonsComputed() {
+      return this.statusChangeButtons.filter((button) =>
+        button.activeStatuses.includes(this.appealStatus)
+      )
+    }
+  },
+  methods: {
+    saveRequest() {
+      alert('Функционал находится в разработке')
+    },
+    print() {
+      alert('Функционал находится в разработке')
+    },
+    changeStatus() {
+      alert('Функционал находится в разработке')
+    }
+  }
+}
+</script>
