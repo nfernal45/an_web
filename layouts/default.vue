036d40377e1aac1bd8c9201881b96eaf63ebf99b
chikenfoot <chikenfoot88@gmail.com>
2019-10-08 18:26:18 +0300

    MZHI-5286. TheAside. sticky in progress

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
   div.flex.justify-center(v-show="isReady" style="background:#f8f8f8")
     the-header
-    el-container.flex-wrap(style="padding-top:80px;position:relative;")
+    el-container#sticky-container.flex-wrap(style="padding-top:80px;position:relative;")
       the-tabs(style="flex-basis:100%;")
-      the-aside.sticky-aside(style="flex-basis:200px;")
+      the-aside#sticky-sidebar.sticky-sidebar(style="flex-basis:200px;position:sticky;")
+        .sidebar__inner
       el-main(style="flex:1")
         nuxt
 </template>
 
 <script>
+import StickySidebar from 'sticky-sidebar'
 import TheHeader from '@/components/TheHeader'
 import TheAside from '@/components/TheAside'
 import TheTabs from '@/components/TheTabs'
@@ -21,13 +23,31 @@ export default {
   },
   data() {
     return {
-      isReady: false
+      isReady: false,
+      offset: {
+        top: 10,
+        bottom: 10
+      }
     }
   },
   mounted() {
     this.$nextTick(() => {
       this.isReady = true
     })
+
+    this.setStickyAside()
+  },
+  methods: {
+    setStickyAside() {
+      if (process.client) {
+        const sidebar = new StickySidebar('#sticky-sidebar', {
+          containerSelector: '#sticky-container',
+          innerWrapperSelector: '.sidebar__inner',
+          topSpacing: 20,
+          bottomSpacing: 20
+        })
+      }
+    }
   }
 }
 </script>
@@ -79,5 +99,15 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+.sticky-sidebar {
+  will-change: min-height;
+}
+
+.sidebar__inner {
+  transform: translate(0, 0); /* For browsers don't support translate3d. */
+  transform: translate3d(0, 0, 0);
+  will-change: position, transform;
 }
 </style>
