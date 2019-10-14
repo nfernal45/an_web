40ab647e71e00e4cae0f9a64a52011fd4bdce03b
aismagilov <aismagilov@c-i-p.ru>
2019-10-14 19:10:52 +0300

    MZHI-5297. add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div.flex.width-100.height-100vh.justify-center.align-center
+    span Загрузка...
+</template>
+<script>
+/* eslint-disable */
+export default {
+  name: 'LoginPage',
+  layout: 'emptyLayout',
+  mounted() {
+    if (this.$route.query.code) {
+      return
+    }
+
+    if (!this.$auth.loggedIn) {
+      this.$auth.loginWith('oauth2')
+    }
+  }
+}
+</script>
