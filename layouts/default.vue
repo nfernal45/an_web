08275f879e5fa2a050629264337d2b3a92f66222
Evgeniy <bongyclan@gmail.com>
2019-10-14 16:48:13 +0300

    MZHI-5294. layouts/default.vue Refactoring.

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,7 +44,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
