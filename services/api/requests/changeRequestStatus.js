78b3452fbc566a3452f777b6471c4f6692e7213c
aismagilov <aismagilov@c-i-p.ru>
2019-10-21 17:21:41 +0300

    MZHI-5294. changeRequestStatus. remove logging

--- services/api/requests/changeRequestStatus.js
+++ services/api/requests/changeRequestStatus.js
@@ -13,7 +13,7 @@ export default async function({
   )
 
   const query = `newStatusId=${nextStatusId}&oldStatusId=${requestStatusId}`
-  console.log(query)
+
   try {
     const { data } = await axiosModule.post(url, query)
     Vue.prototype.$notify.success({
