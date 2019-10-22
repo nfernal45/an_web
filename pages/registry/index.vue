b6d806f9bf4f19e1e2c97c248e9236146f04ba34
chikenfoot <chikenfoot88@gmail.com>
2019-10-22 12:02:22 +0300

    MZHI-5289. request list search. add search

--- pages/registry/index.vue
+++ pages/registry/index.vue
@@ -1,8 +1,31 @@
 <template lang="pug">
   div
-    h1 Реестр заявлений
+    h1 Список заявлений на внесение изменений в реестр лицензий субъекта
+    registry-search-form(@onSearch="searchRequests" :isSearchLoading="isSearchLoading").mt-10
 </template>
 <script>
-export default {}
+import RegistrySearchForm from '@/components/registry/registrySearchForm'
+import fetchRequestsList from '@/services/api/requests/fetchRequestsList'
+export default {
+  layout: 'registry',
+  components: {
+    RegistrySearchForm
+  },
+  data() {
+    return {
+      isSearchLoading: false
+    }
+  },
+  methods: {
+    async searchRequests(searchParams) {
+      this.isSearchLoading = true
+      this.requestsList = await fetchRequestsList({
+        axiosModule: this.$axios,
+        searchParams
+      })
+      this.isSearchLoading = false
+    }
+  }
+}
 </script>
 <style lang="sass"></style>
