f0d5f8f71af0874a18909547fc8cd21dadd26ea2
chikenfoot <chikenfoot88@gmail.com>
2019-10-22 16:37:16 +0300

    MZHI-5291. add requests list table

--- components/registry/registryRequestsTable/registryRequestsTable.vue
+++ components/registry/registryRequestsTable/registryRequestsTable.vue
@@ -1,10 +1,65 @@
 <template lang="pug">
- div
-   h2 Таблица заявлений
+  el-table(:data="computedRequestsList" border style="width: 100%")
+    el-table-column(prop="requestDate" label="Дата подачи заявления" width="120")
+    el-table-column(prop="regnum" label="Рег. №" width="120")
+    el-table-column(prop="licenseeFullname" label="Заявитель")
+    el-table-column(prop="addressName" label="Адрес МКД")
+    el-table-column(prop="typeId" label="Цель обращения" width="200")
+    el-table-column(prop="statusId" label="Статус заявления" width="160")
+    el-table-column(fixed="right" label="" width="60")
+      template(slot-scope="scope")
+        el-button.d-flex.justify-center.align-center(
+          type="primary"
+          size="small"
+          style="width:40px;height:40px;padding:0;"
+          @click="openRequest(scope.row.requestId)"
+        )
+          font-awesome-icon(icon="pen" style="margin:0;")
 </template>
 <script>
 export default {
-  name: 'RegistryRequestsTable'
+  name: 'RegistryRequestsTable',
+  props: {
+    requestsList: {
+      type: Array,
+      default: () => []
+    },
+    requestStatusesOptions: {
+      type: Array,
+      default: () => []
+    },
+    requestTypesOptions: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    computedRequestsList() {
+      return this.requestsList.map((request) => {
+        return {
+          requestDate: request.requestDate,
+          regnum: request.regnum,
+          licenseeFullname: request.licenseeFullname,
+          addressName: request.addressName,
+          typeId: this.requestTypesOptions.find(
+            (type) => type.typeId === request.typeId
+          ).typeName,
+          requestId: request.requestId,
+          statusId: this.requestStatusesOptions.find(
+            (status) => status.statusId === request.requestStatusId
+          ).statusName
+        }
+      })
+    }
+  },
+  methods: {
+    openRequest(requestId) {
+      this.$router.push({
+        name: 'request-id-main',
+        params: { id: requestId }
+      })
+    }
+  }
 }
 </script>
 <style lang="sass"></style>
--- pages/registry/index.vue
+++ pages/registry/index.vue
@@ -2,19 +2,35 @@
   div
     h1 Список заявлений на внесение изменений в реестр лицензий субъекта
     registry-search-form(@onSearch="searchRequests" :isSearchLoading="isSearchLoading").mt-10
+    registry-requests-table(
+      :requests-list="requestsList"
+      :requestTypesOptions="requestTypesOptions"
+      :requestStatusesOptions="requestStatusesOptions"
+    )
 </template>
 <script>
 import RegistrySearchForm from '@/components/registry/registrySearchForm'
+import RegistryRequestsTable from '@/components/registry/registryRequestsTable'
 import fetchRequestsList from '@/services/api/requests/fetchRequestsList'
+import fetchRequestTypesOptions from '@/services/api/references/fetchRequestTypesOptions'
+import fetchRequestStatusesOptions from '@/services/api/references/fetchRequestStatusesOptions'
 export default {
   layout: 'registry',
   components: {
-    RegistrySearchForm
+    RegistrySearchForm,
+    RegistryRequestsTable
   },
   data() {
     return {
-      isSearchLoading: false
+      isSearchLoading: false,
+      requestsList: [],
+      requestTypesOptions: [],
+      requestStatusesOptions: []
     }
+  },
+  mounted() {
+    this.fetchRequestTypesOptions()
+    this.fetchRequestStatusesOptions()
   },
   methods: {
     async searchRequests(searchParams) {
@@ -24,6 +40,16 @@ export default {
         searchParams
       })
       this.isSearchLoading = false
+    },
+    async fetchRequestTypesOptions() {
+      this.requestTypesOptions = await fetchRequestTypesOptions({
+        axiosModule: this.$axios
+      })
+    },
+    async fetchRequestStatusesOptions() {
+      this.requestStatusesOptions = await fetchRequestStatusesOptions({
+        axiosModule: this.$axios
+      })
     }
   }
 }
