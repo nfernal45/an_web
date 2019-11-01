7900c3b3983b6831eed5b933ebceab19d6b944c1
aismagilov <aismagilov@c-i-p.ru>
2019-11-01 15:12:21 +0300

    MZHI-5286. add global secondary color. add background for statuses buttons

--- components/TheAside/TheAsideStatusesButtons/TheAsideStatusesButtons.vue
+++ components/TheAside/TheAsideStatusesButtons/TheAsideStatusesButtons.vue
@@ -1,28 +1,28 @@
 <template lang="pug">
-  div(v-show='request.requestId'
-      v-loading='isStatusesButtonsLoading' 
-      style='margin-top: 40px; min-height: 38px;')
+  div(
+    id='status-buttons'
+    v-show='request.requestId'
+    style='margin-top: 40px; min-height: 38px;'
+  )
     ul(:class='styles["list"]')
       li(v-for='(button, index) in requestStatuses' :key='index' :class='styles["list-item"]')
-        el-button(type='warning' 
-                  plain 
-                  :class='styles["list-button", "status-button"]' @click='onStatusChange(button.statusId)') {{ button.buttonName }}
+        el-button(
+          type='warning' 
+          plain 
+          :class='styles["list-button", "status-button"]'
+          @click='onStatusChange(button.statusId)'
+        ) {{ button.buttonName }}
 
 </template>
 <script>
 import { mapState, mapActions } from 'vuex'
-import styles from '../TheAside.module.sass?module'
+import styles from './TheAsideStatusesButtons.module.sass?module'
 import { actionTypes as requestActionTypes } from '@/store/types/request'
 
 const moduleName = 'request'
 
 export default {
   name: 'TheAsideStatusesButtons',
-  data() {
-    return {
-      isStatusesButtonsLoading: false
-    }
-  },
   computed: {
     ...mapState({
       request: (state) => state.request.request,
@@ -38,9 +38,13 @@ export default {
       changeRequestStatus: requestActionTypes.CHANGE_REQUEST_STATUS
     }),
     async onStatusChange(nextStatusId) {
-      this.isStatusesButtonsLoading = true
+      const loading = this.$loading({
+        target: '#status-buttons',
+        lock: true,
+        customClass: this.styles['el-loading-mask']
+      })
       await this.changeRequestStatus(nextStatusId)
-      this.isStatusesButtonsLoading = false
+      loading.close()
     }
   }
 }
