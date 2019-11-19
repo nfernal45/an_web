43fd8f14042103945dd43cf08c8b07d03da28a41
aismagilov <aismagilov@c-i-p.ru>
2019-11-19 19:21:38 +0300

    MZHI-5297. add status change error handler

--- plugins/axios.js
+++ plugins/axios.js
@@ -11,6 +11,7 @@ export default function({ $axios, $auth, base, redirect, route }) {
 
   $axios.onError((error) => {
     const code = parseInt(error.response && error.response.status)
+    const errorMsg = error.response ? error.response.data.error : ''
 
     if (code === 404) {
       throw error
@@ -19,18 +20,32 @@ export default function({ $axios, $auth, base, redirect, route }) {
     const originalRequest = error.config
 
     if (code === 400 || code === 401) {
-      originalRequest.__isRetryRequest = true
-      return new Promise((resolve, reject) => {
-        refreshToken({ authModule: $auth, axiosModule: $axios })
+      if (errorMsg === 'invalid_token') {
+        return new Promise((resolve, reject) => {
+          refreshToken({ authModule: $auth, axiosModule: $axios })
+            .then(() => {
+              if (originalRequest.params && originalRequest.params.token)
+                originalRequest.params.token = $auth
+                  .getToken('oauth2')
+                  .split(' ')[1]
+              originalRequest.headers.Authorization = $auth.getToken('oauth2')
+              resolve()
+            })
+            .catch((error) => {
+              logout({
+                authModule: $auth,
+                axiosModule: $axios,
+                baseRoute: base,
+                currentRoute: route.path,
+                redirectFunction: redirect
+              })
+              reject(error)
+            })
+        })
           .then(() => {
-            if (originalRequest.params && originalRequest.params.token)
-              originalRequest.params.token = $auth
-                .getToken('oauth2')
-                .split(' ')[1]
-            originalRequest.headers.Authorization = $auth.getToken('oauth2')
-            resolve()
+            return $axios.request(originalRequest)
           })
-          .catch((error) => {
+          .catch(() => {
             logout({
               authModule: $auth,
               axiosModule: $axios,
@@ -38,21 +53,11 @@ export default function({ $axios, $auth, base, redirect, route }) {
               currentRoute: route.path,
               redirectFunction: redirect
             })
-            reject(error)
           })
-      })
-        .then(() => {
-          return $axios.request(originalRequest)
-        })
-        .catch(() => {
-          logout({
-            authModule: $auth,
-            axiosModule: $axios,
-            baseRoute: base,
-            currentRoute: route.path,
-            redirectFunction: redirect
-          })
-        })
+      } else {
+        // TODO: реализовать правильную обработку ошибки
+        throw error
+      }
     }
   })
 }
