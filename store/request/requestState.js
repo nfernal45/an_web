2c125265c4f91b383190a90be6c6b5fc4c618ef8
aismagilov <aismagilov@c-i-p.ru>
2020-01-20 11:44:41 +0300

    MZHI-5614. add attached doc type check on status change

--- store/request/requestState.js
+++ store/request/requestState.js
@@ -8,5 +8,24 @@ export default () => ({
   requestStatuses: [],
   gfAttachedDocsByRequestIdDefault: null,
   gfQueriedDocsByRequestIdDefault: null,
-  gfRefusalReasonRequestIdDefault: null
+  gfRefusalReasonRequestIdDefault: null,
+  docTypesConstants: {
+    ExtractOrReceiptOnRegistrationOfApplication: {
+      typeId: 16,
+      warehouseName: 'Опись принятых от заявителя документов'
+    },
+    NotificationOfProvisionOfServices: {
+      typeId: 20,
+      warehouseName: 'Уведомление о предоставлении услуги'
+    },
+    ProvideServicesRefuseDecision: {
+      typeId: 17,
+      warehouseName: 'Решение об отказе в предоставлении услуги'
+    },
+    DecisionOnSuspensionOfProvidingTheStateService: {
+      typeId: 18,
+      warehouseName:
+        'Уведомление о необходимости устранения нарушений и (или) досыла документов'
+    }
+  }
 })
