9e3faec34793b1bdae4fe793447e20ea8d19bc99
Evgeniy <bongyclan@gmail.com>
2020-01-29 12:43:09 +0300

    MZHI-6177. fix validation

--- services/requestValidation.js
+++ services/requestValidation.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 
 export const validation = function(request) {
+  // if (request.reprFullname.length || request.reprShortname.length)
+
   if (request.licenseeType === 'L') {
     if (String(request.licenseeInn).length !== 10) {
       showWarning(
@@ -25,34 +27,41 @@ export const validation = function(request) {
       )
       return false
     }
-  } else if (request.reprType === 'L') {
-    if (String(request.reprInn).length !== 10) {
-      showWarning(
-        'Для данного типа представителя заявителя допустимая длина поля "ИНН": 10 символов.'
-      )
-      return false
-    } else if (String(request.reprOgrn).length !== 13) {
-      showWarning(
-        'Для данного типа представителя заявителя допустимая длина поля "ОГРН": 13 символов.'
-      )
-      return false
-    }
-    return false
-  } else if (request.reprType === 'I') {
-    if (String(request.reprInn).length !== 12) {
-      showWarning(
-        'Для данного типа представителя заявителя допустимая длина поля "ИНН": 12 символов.'
-      )
-      return false
-    } else if (String(request.reprOgrn).length !== 15) {
-      showWarning(
-        'Для данного типа представителя заявителя допустимая длина поля "ОГРНИП": 15 символов.'
-      )
-      return false
-    }
   } else {
-    showWarning('Необходимо выбрать тип заявителя или представителя заявителя.')
+    showWarning('Необходимо выбрать тип заявителя.')
     return false
+  }
+
+  if (request.reprFullname || request.reprShortname) {
+    if (request.reprType === 'L') {
+      if (String(request.reprInn).length !== 10) {
+        showWarning(
+          'Для данного типа представителя заявителя допустимая длина поля "ИНН": 10 символов.'
+        )
+        return false
+      } else if (String(request.reprOgrn).length !== 13) {
+        showWarning(
+          'Для данного типа представителя заявителя допустимая длина поля "ОГРН": 13 символов.'
+        )
+        return false
+      }
+      return false
+    } else if (request.reprType === 'I') {
+      if (String(request.reprInn).length !== 12) {
+        showWarning(
+          'Для данного типа представителя заявителя допустимая длина поля "ИНН": 12 символов.'
+        )
+        return false
+      } else if (String(request.reprOgrn).length !== 15) {
+        showWarning(
+          'Для данного типа представителя заявителя допустимая длина поля "ОГРНИП": 15 символов.'
+        )
+        return false
+      }
+    } else {
+      showWarning('Необходимо выбрать тип представителя заявителя.')
+      return false
+    }
   }
 
   return true
