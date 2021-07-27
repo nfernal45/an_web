3513b12634ffaefd736d3d47d32ee6c18dc1f633
alyonium <alyonium2.0@gmail.com>
2021-07-27 15:21:53 +0300

    EISMGI-3536

--- store/request/requestActions.js
+++ store/request/requestActions.js
@@ -37,7 +37,7 @@ export default {
         requestId: currentRequestId
       })
 
-      if (request.requestStatusId === 9 || request.requestStatusId === 10) {
+      if (request.gfAbeyancesByRequestId.length && request.abeyance === 'N') {
         request.abeyance = 'Y'
       }
 
