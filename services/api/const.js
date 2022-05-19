b1e40ae36be32d07b06606a55f665ed1a336bf7d
Nikita Gorodilov <nikita.gorodilov@fors.ru>
2022-05-19 16:50:26 +0300

    EISMGI-4800 - Изменен процесс ввода даты при запросе документа

---
+++ services/api/const.js
@@ -0,0 +1,3 @@
+// Id записи параметра для ввода значения в поле "Дата разрешения на ввод объекта в эксплуатацию"
+// см. файл fetchRequiredInterParams.js
+export const INF_IN_EXPL_DATE_PERMISSION_INFER_PARAM_ID = 21
