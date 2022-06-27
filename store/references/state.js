1db61f585c06316b35c85b0824c671af2385beef
Nikita Gorodilov <nikita.gorodilov@fors.ru>
2022-06-27 13:36:24 +0300

    EISMGI-4918 - Добавлена фильтрация по адресам

--- store/references/state.js
+++ store/references/state.js
@@ -1,6 +1,6 @@
 export default () => ({
   requestStatusesOptions: [],
-  globalSearchFilters: '',
+  globalSearchFilters: {},
   globalSearchFiltersSettings: {},
   globalSearchAddressFiltersSettings: {}
 })
